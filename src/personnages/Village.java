01b1b56f9299d90938183ece22dd20f8992d8273
ichelmann ali <chl3685b@ens.fsi>
2022-10-07 08:34:39 +0200

    TP2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,13 +2,42 @@ package personnages;
 
 public class Village {
 	private String nom;
+	private villageois[] gaulois;
+	private int nbVillageois = 0;
 	
-	public Village(String nom) {
+	
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom=nom;
+		this.gaulois = new villageois[nbVillageoisMaximum];
 	}
 	
+	public void ajouterHabitant(String gaulois) {
+		if(nbVillageois<nbVillageoisMaximum) {
+			villageois[nbVillageois++]=gaulois;
+		}		
+	}
+	
+	public String trouverHabitant(int numVillageois) {
+		return villageois[numVillageois];
+		
+	}
+	
+	 public void setChef(Chef chef) { 
+		 this.chef = chef; 
+	 }
+	 
 	public String getNom() {
 		return nom;
+	}
+	
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irreductibles",30);
+		Gaulois gaulois = village.trouverHabitant(30);
+		
+		
+		
+		
 	}
 	
 
