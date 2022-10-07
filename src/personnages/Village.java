c3d0edd32e52ad3858060026dcbc5c4c5d69ef07
ichelmann ali <chl3685b@ens.fsi>
2022-10-07 09:37:20 +0200

    TP2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,43 +2,39 @@ package personnages;
 
 public class Village {
 	private String nom;
-	private villageois[] gaulois;
+	Gaulois[] villageois;
 	private int nbVillageois = 0;
+	private int nbVillageoisMaximum;
 	
 	
-	public Village(String nom, int nbVillageoisMaximum) {
-		this.nom=nom;
-		this.gaulois = new villageois[nbVillageoisMaximum];
+	public Village(String nom,int nbVillageoisMaximum) {
+		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
 	
 	public void ajouterHabitant(String gaulois) {
-		if(nbVillageois<nbVillageoisMaximum) {
-			villageois[nbVillageois++]=gaulois;
-		}		
+		if (nbVillageois < nbVillageoisMaximum) {
+			Gaulois[nbVillageois++] = gaulois;
+		}
 	}
-	
+
 	public String trouverHabitant(int numVillageois) {
-		return villageois[numVillageois];
-		
+		return Gaulois[numVillageois];
+
 	}
-	
-	 public void setChef(Chef chef) { 
-		 this.chef = chef; 
-	 }
-	 
+
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+
 	public String getNom() {
 		return nom;
 	}
-	
-	
-	public static void main(String[] args) {
-		Village village = new Village("Village des Irreductibles",30);
-		Gaulois gaulois = village.trouverHabitant(30);
-		
-		
-		
-		
-	}
-	
 
+
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irreductibles", 30);
+		Gaulois gaulois = village.trouverHabitant(30);
+
+	}
 }
