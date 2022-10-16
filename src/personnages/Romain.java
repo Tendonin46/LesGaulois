7abb25a06b815e7306ba7f5ea76adbbd418c9596
Tendonin46 <ali.ichelmman@hotmail.com>
2022-10-16 20:17:27 +0200

    Enum

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,8 +1,10 @@
 package personnages;
 
 public class Romain {
-	private String nom;
+	private static String nom;
 	private int force;
+	private static Equipement[] equipements = new Equipement[2];
+	private static int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		this.nom=nom;
@@ -31,8 +33,33 @@ public class Romain {
 		}
 		assert x<=force: "La force n'a pas diminué";
 	}
+	
+	public static void sEquiper(Equipement Equip) {
+		switch(nbEquipement) {
+			case 2:
+				System.out.println("Le soldat "+nom+" est déjà bien protégé.");
+				break;
+			case 1:
+				if(Equip==equipements[0]) {
+					System.out.println("Le soldat "+nom+" possède déjà un "+Equip+"!");
+				}
+				else {
+					equipements[nbEquipement++]=Equip;
+					System.out.println("Le soldat "+nom+" s'équipe avec un "+Equip+".");
+				}
+				break;	
+			default:
+				equipements[nbEquipement++]=Equip;
+				System.out.println("Le soldat "+nom+" s'équipe avec un "+Equip+".");
+				break;
+		}
+	}
+	
 	public static void main(String[] args) {
 		Romain Minus=new Romain("Minus",6);
-		
+		sEquiper(Equipement.CASQUE);
+		sEquiper(Equipement.CASQUE);
+		sEquiper(Equipement.BOUCLIER);
+		sEquiper(Equipement.CASQUE);
 	}
 }
